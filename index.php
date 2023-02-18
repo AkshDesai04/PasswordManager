5671befd001ab4e8ec02ae85456a5f006a6988a9
AkshDesai04 <dev.akshdesai@gmail.com>
2023-02-18 14:28:38 +0530

    changed extentions to php and added redirects from index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
-    <head><title>MyPass</title></head>
+    <head>
+        <title>MyPass</title>
+    </head>
     <body>
         <h1>Welcome to MyPass!</h1>
-        <button type="button">Log In</button>
-        <button type="button">Register</button>
+        <button type="button" onClick="document.location.href='./login.php'">Log In</button>
+        <button type="button" onClick="document.location.href='./signup.php'">Register</button>g
     </body>
 </html>
--- login.php
+++ login.php
@@ -1,15 +1,50 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Login - MyPass</title>
-    </head>
-    <body>
-        <h1>Let's get you logged in!</h1>
-        <label>User ID: </label>
-        <input type="text"><br>
-        <label>Password: </label>
-        <input type="text"><br>
-        <input type="checkbox"> <label>Remember Me</label><br>
-        <input type="submit" text = "GO">
-    </body>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Login</title>
+		<link rel="stylesheet" href="./style.css" />
+	</head>
+	<body>
+		<section class="form-container">
+			<div class="title-container">
+				<h2 class="title">Welcome Back</h2>
+				<p class="sub-title">Let&apos;s get you signed in</p>
+			</div>
+			<form action="">
+				<div class="input-container">
+					<label for="email">Email</label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						placeholder="Enter you email"
+					/>
+				</div>
+				<div class="input-container">
+					<label for="password">Password</label>
+					<input
+						type="password"
+						name="password"
+						id="password"
+						placeholder="Enter you password"
+					/>
+				</div>
+				<div class="remember-forgot">
+					<div class="remember">
+						<input type="checkbox" name="remember" id="remember" />
+						<label for="remember">Remember for 30 days</label>
+					</div>
+					<p class="forgot">Forgot password?</p>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+			<p class="signup">
+				Don&apos;t have an account? <a href="/signup.html">Sign up</a>
+			</p>
+		</section>
+		<section class="image"></section>
+	</body>
 </html>
